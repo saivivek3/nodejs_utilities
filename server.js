42867ca2021c66322b5df2c67fb3a624fcab5afb
saivivek <saivivek.ravi@databeat.io>
2024-05-27 16:14:55 +0530

    added code

--- server.js
+++ server.js
@@ -296,395 +296,167 @@
 //   console.log("Server on...");
 // });
 
-// const express = require("express");
-// const multer = require("multer");
-// const path = require("path");
-// const fs = require("fs");
-// const { createObjectCsvStringifier } = require("csv-writer");
-// const cors = require("cors");
-// const app = express();
-
-// // Use CORS middleware
-// app.use(cors());
-
-// // Ensure the uploads directory exists
-// const uploadDir = "uploads/";
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir);
-// }
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
-// // File filter to only accept JSON files
-// const fileFilter = (req, file, cb) => {
-//   const filetypes = /json/;
-//   const mimetype = filetypes.test(file.mimetype);
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-//   if (mimetype && extname) {
-//     return cb(null, true);
-//   } else {
-//     cb(new Error("Only JSON files are allowed!"));
-//   }
-// };
-
-// const uploadStorage = multer({
-//   storage: storage,
-//   fileFilter: fileFilter,
-// });
-
-// // Single file
-// app.post("/upload/single", uploadStorage.single("file"), (req, res) => {
-//   if (req.file) {
-//     // Read the uploaded JSON file
-//     fs.readFile(req.file.path, "utf8", (err, data) => {
-//       if (err) {
-//         return res.status(500).send("Error reading the JSON file");
-//       }
-//       try {
-//         const jsonData = JSON.parse(data);
-//         console.log(jsonData);
-
-//         // Function to find the first array with two or more items
-//         const findFirstArray = (obj) => {
-//           let result = null;
-//           const findArray = (object) => {
-//             for (let key in object) {
-//               if (object.hasOwnProperty(key)) {
-//                 if (Array.isArray(object[key]) && object[key].length >= 1) {
-//                   result = object[key];
-//                   return;
-//                 } else if (typeof object[key] === "object") {
-//                   findArray(object[key]);
-//                   if (result) return;
-//                 }
-//               }
-//             }
-//           };
-//           findArray(obj);
-//           return result;
-//         };
-
-//         const firstArray = findFirstArray(jsonData);
-//         if (!firstArray) {
-//           return res.status(400).send("No array with two or more items found.");
-//         }
-
-//         // Function to flatten nested JSON
-//         const flattenObject = (obj, prefix = "") => {
-//           let flat = {};
-//           for (let key in obj) {
-//             if (obj.hasOwnProperty(key)) {
-//               if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
-//                 let nestedObject = flattenObject(obj[key], prefix + key + ".");
-//                 for (let nestedKey in nestedObject) {
-//                   if (nestedObject.hasOwnProperty(nestedKey)) {
-//                     flat[nestedKey] = nestedObject[nestedKey];
-//                   }
-//                 }
-//               } else if (Array.isArray(obj[key])) {
-//                 obj[key].forEach((item, index) => {
-//                   if (typeof item === "object") {
-//                     let nestedArrayObject = flattenObject(
-//                       item,
-//                       prefix + key + "[" + index + "]."
-//                     );
-//                     for (let nestedArrayKey in nestedArrayObject) {
-//                       if (nestedArrayObject.hasOwnProperty(nestedArrayKey)) {
-//                         flat[nestedArrayKey] =
-//                           nestedArrayObject[nestedArrayKey];
-//                       }
-//                     }
-//                   } else {
-//                     flat[prefix + key + "[" + index + "]"] = item;
-//                   }
-//                 });
-//               } else {
-//                 flat[prefix + key] = obj[key];
-//               }
-//             }
-//           }
-//           return flat;
-//         };
-
-//         // Function to dynamically create CSV stringifier
-//         const createDynamicCsvStringifier = (data) => {
-//           const flattenedData = data.map((item) => flattenObject(item));
-//           const headers = Array.from(
-//             new Set(flattenedData.flatMap((item) => Object.keys(item)))
-//           ).map((key) => ({ id: key, title: key }));
-
-//           return createObjectCsvStringifier({
-//             header: headers,
-//           });
-//         };
-
-//         // Flatten and prepare data for CSV writing
-//         const flatData = firstArray.map((item) => flattenObject(item));
-
-//         // Create CSV stringifier
-//         const csvStringifier = createDynamicCsvStringifier(flatData);
-//         const header = csvStringifier.getHeaderString();
-//         const records = csvStringifier.stringifyRecords(flatData);
-
-//         // Set the response headers for file download
-//         res.setHeader("Content-disposition", "attachment; filename=data.csv");
-//         res.set("Content-Type", "text/csv");
-
-//         // Send the CSV content as the response
-//         res.send(header + records);
-//       } catch (err) {
-//         return res.status(400).send("Error parsing the JSON file");
-//       }
-//     });
-//   } else {
-//     return res.status(400).send("Error: Only JSON files are allowed!");
-//   }
-// });
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   if (err) {
-//     return res.status(400).send(err.message);
-//   }
-//   next();
-// });
-
-// app.listen(3000 || process.env.PORT, () => {
-//   console.log("Server on...");
-// });
-
-// const express = require("express");
-// const multer = require("multer");
-// const path = require("path");
-// const fs = require("fs");
-// const { createObjectCsvStringifier } = require("csv-writer");
-// const cors = require("cors");
-// const app = express();
-
-// // Use CORS middleware
-// app.use(cors());
-
-// // Ensure the uploads directory exists
-// const uploadDir = "uploads/";
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir);
-// }
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
-// // File filter to only accept JSON files
-// const fileFilter = (req, file, cb) => {
-//   const filetypes = /json/;
-//   const mimetype = filetypes.test(file.mimetype);
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-//   if (mimetype && extname) {
-//     return cb(null, true);
-//   } else {
-//     cb(new Error("Only JSON files are allowed!"));
-//   }
-// };
-
-// const uploadStorage = multer({
-//   storage: storage,
-//   fileFilter: fileFilter,
-// });
-
-// // Single file
-// app.post("/upload/single", uploadStorage.single("file"), (req, res) => {
-//   if (req.file) {
-//     // Read the uploaded JSON file
-//     fs.readFile(req.file.path, "utf8", (err, data) => {
-//       if (err) {
-//         return res.status(500).send("Error reading the JSON file");
-//       }
-//       try {
-//         const jsonData = JSON.parse(data);
-//         console.log(jsonData);
-
-//         // Function to flatten nested JSON
-//         const flattenObject = (obj, prefix = "") => {
-//           let flat = {};
-//           for (let key in obj) {
-//             if (obj.hasOwnProperty(key)) {
-//               if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
-//                 let nestedObject = flattenObject(obj[key], prefix + key + ".");
-//                 for (let nestedKey in nestedObject) {
-//                   if (nestedObject.hasOwnProperty(nestedKey)) {
-//                     flat[nestedKey] = nestedObject[nestedKey];
-//                   }
-//                 }
-//               } else if (Array.isArray(obj[key])) {
-//                 obj[key].forEach((item, index) => {
-//                   if (typeof item === "object") {
-//                     let nestedArrayObject = flattenObject(
-//                       item,
-//                       prefix + key + "[" + index + "]."
-//                     );
-//                     for (let nestedArrayKey in nestedArrayObject) {
-//                       if (nestedArrayObject.hasOwnProperty(nestedArrayKey)) {
-//                         flat[nestedArrayKey] =
-//                           nestedArrayObject[nestedArrayKey];
-//                       }
-//                     }
-//                   } else {
-//                     flat[prefix + key + "[" + index + "]"] = item;
-//                   }
-//                 });
-//               } else {
-//                 flat[prefix + key] = obj[key];
-//               }
-//             }
-//           }
-//           return flat;
-//         };
-
-//         // Find the last array key and its value
-//         let lastArrayKey = null;
-//         let lastArrayData = null;
-
-//         const findLastArrayKey = (obj) => {
-//           for (let key in obj) {
-//             if (obj.hasOwnProperty(key)) {
-//               if (Array.isArray(obj[key])) {
-//                 lastArrayKey = key;
-//                 lastArrayData = obj[key];
-//               } else if (typeof obj[key] === "object") {
-//                 findLastArrayKey(obj[key]);
-//               }
-//             }
-//           }
-//         };
-
-//         findLastArrayKey(jsonData);
-
-//         if (!lastArrayKey || !lastArrayData) {
-//           return res.status(400).send("No array found in the JSON file");
-//         }
-
-//         // Function to dynamically create CSV stringifier
-//         const createDynamicCsvStringifier = (data) => {
-//           const flattenedData = data.map((item) => flattenObject(item));
-//           const headers = Array.from(
-//             new Set(flattenedData.flatMap((item) => Object.keys(item)))
-//           ).map((key) => ({ id: key, title: key }));
-
-//           return createObjectCsvStringifier({
-//             header: headers,
-//           });
-//         };
-
-//         // Flatten and prepare data for CSV writing
-//         const flatData = lastArrayData.map((item) => flattenObject(item));
-
-//         // Create CSV stringifier
-//         const csvStringifier = createDynamicCsvStringifier(flatData);
-//         const header = csvStringifier.getHeaderString();
-//         const records = csvStringifier.stringifyRecords(flatData);
-
-//         // Set the response headers for file download
-//         res.setHeader("Content-disposition", "attachment; filename=test.csv");
-//         res.set("Content-Type", "text/csv");
-
-//         // Send the CSV content as the response
-//         res.send(header + records);
-//       } catch (err) {
-//         return res.status(400).send("Error parsing the JSON file");
-//       }
-//     });
-//   } else {
-//     return res.status(400).send("Error: Only JSON files are allowed!");
-//   }
-// });
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   if (err) {
-//     return res.status(400).send(err.message);
-//   }
-//   next();
-// });
-
-// app.listen(3000 || process.env.PORT, () => {
-//   console.log("Server on...");
-// });
-import express from "express";
-import multer from "multer";
-import { readFile, unlink } from "fs";
-import { Parser } from "json2csv";
-import { flatten } from "flat";
-import cors from "cors";
-
-// Initialize Express
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const { createObjectCsvStringifier } = require("csv-writer");
+const cors = require("cors");
 const app = express();
-const port = 3000;
 
+// Use CORS middleware
 app.use(cors());
-// Set up Multer for file uploads
-const upload = multer({ dest: "uploads/" });
 
-// Route to handle file upload and conversion
-app.post("/upload", upload.single("file"), (req, res) => {
-  // Ensure a file was uploaded
+// Ensure the uploads directory exists
+const uploadDir = "uploads/";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
-  // Use CORS middleware
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
-  if (!req.file) {
-    return res.status(400).send("No file uploaded.");
+// File filter to only accept JSON files
+const fileFilter = (req, file, cb) => {
+  const filetypes = /json/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Only JSON files are allowed!"));
   }
+};
 
-  // Read the uploaded JSON file
-  readFile(req.file.path, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send("Error reading the file.");
-    }
-
-    try {
-      // Parse JSON data
-      const jsonData = JSON.parse(data);
-
-      // Flatten JSON data
-      const flattenedData = flatten(jsonData);
-
-      // Convert flattened data to CSV
-      const json2csvParser = new Parser();
-      const csv = json2csvParser.parse(flattenedData);
-
-      // Send CSV as a response
-      res.header("Content-Type", "text/csv");
-      res.attachment("data.csv");
-      res.send(csv);
-    } catch (err) {
-      res.status(500).send("Error processing the file.");
-    } finally {
-      // Clean up uploaded file
-      unlink(req.file.path, (err) => {
-        if (err) console.error("Error deleting the file:", err);
-      });
-    }
-  });
+const uploadStorage = multer({
+  storage: storage,
+  fileFilter: fileFilter,
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// Single file
+app.post("/upload/single", uploadStorage.single("file"), (req, res) => {
+  if (req.file) {
+    // Read the uploaded JSON file
+    fs.readFile(req.file.path, "utf8", (err, data) => {
+      if (err) {
+        return res.status(500).send("Error reading the JSON file");
+      }
+      try {
+        const jsonData = JSON.parse(data);
+        console.log(jsonData);
+
+        // Function to find the first array with two or more items
+        const findFirstArray = (obj) => {
+          let result = null;
+          const findArray = (object) => {
+            for (let key in object) {
+              if (object.hasOwnProperty(key)) {
+                if (Array.isArray(object[key]) && object[key].length >= 1) {
+                  result = object[key];
+                  return;
+                } else if (typeof object[key] === "object") {
+                  findArray(object[key]);
+                  if (result) return;
+                }
+              }
+            }
+          };
+          findArray(obj);
+          return result;
+        };
+
+        const firstArray = findFirstArray(jsonData);
+        if (!firstArray) {
+          return res.status(400).send("No array with two or more items found.");
+        }
+
+        // Function to flatten nested JSON
+        const flattenObject = (obj, prefix = "") => {
+          let flat = {};
+          for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+              if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+                let nestedObject = flattenObject(obj[key], prefix + key + ".");
+                for (let nestedKey in nestedObject) {
+                  if (nestedObject.hasOwnProperty(nestedKey)) {
+                    flat[nestedKey] = nestedObject[nestedKey];
+                  }
+                }
+              } else if (Array.isArray(obj[key])) {
+                obj[key].forEach((item, index) => {
+                  if (typeof item === "object") {
+                    let nestedArrayObject = flattenObject(
+                      item,
+                      prefix + key + "[" + index + "]."
+                    );
+                    for (let nestedArrayKey in nestedArrayObject) {
+                      if (nestedArrayObject.hasOwnProperty(nestedArrayKey)) {
+                        flat[nestedArrayKey] =
+                          nestedArrayObject[nestedArrayKey];
+                      }
+                    }
+                  } else {
+                    flat[prefix + key + "[" + index + "]"] = item;
+                  }
+                });
+              } else {
+                flat[prefix + key] = obj[key];
+              }
+            }
+          }
+          return flat;
+        };
+
+        // Function to dynamically create CSV stringifier
+        const createDynamicCsvStringifier = (data) => {
+          const flattenedData = data.map((item) => flattenObject(item));
+          const headers = Array.from(
+            new Set(flattenedData.flatMap((item) => Object.keys(item)))
+          ).map((key) => ({ id: key, title: key }));
+
+          return createObjectCsvStringifier({
+            header: headers,
+          });
+        };
+
+        // Flatten and prepare data for CSV writing
+        const flatData = firstArray.map((item) => flattenObject(item));
+
+        // Create CSV stringifier
+        const csvStringifier = createDynamicCsvStringifier(flatData);
+        const header = csvStringifier.getHeaderString();
+        const records = csvStringifier.stringifyRecords(flatData);
+
+        // Set the response headers for file download
+        res.setHeader("Content-disposition", "attachment; filename=data.csv");
+        res.set("Content-Type", "text/csv");
+
+        // Send the CSV content as the response
+        res.send(header + records);
+      } catch (err) {
+        return res.status(400).send("Error parsing the JSON file");
+      }
+    });
+  } else {
+    return res.status(400).send("Error: Only JSON files are allowed!");
+  }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    return res.status(400).send(err.message);
+  }
+  next();
+});
+
+app.listen(3000 || process.env.PORT, () => {
+  console.log("Server on...");
 });
